f02795a92a3939a4e9aa4b0adbc38f8229f19d5e
braverokmc79 <braverokmc79@gmail.com>
2023-03-17 20:33:53 +0900

    폼 초기화 풀이

--- 2-react-exe/js/main.js
+++ 2-react-exe/js/main.js
@@ -7,10 +7,28 @@ class App extends React.Component {
     };
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    console.log("Tood :Handle Submit : ", this.state.searchKeyword);
+  }
+
+  handleReset() {
+    this.setState(
+      () => {
+        return { searchKeyword: "" };
+      },
+      () => {
+        console.log("TODO : handleReset ", this.state.searchKeyword);
+      }
+    );
+  }
   handleChangeInput(event) {
     // this.state.searchKeyword = event.target.value;
     // this.forceUpdate();
-
+    if (searchKeyword.length <= 0) {
+      return this.handleReset();
+    }
     this.setState({
       searchKeyword: event.target.value,
     });
@@ -29,7 +47,10 @@ class App extends React.Component {
           <h2 className="container">검색</h2>
         </header>
         <div className="container">
-          <form>
+          <form
+            onSubmit={(event) => this.handleSubmit(event)}
+            onReset={() => this.handleReset()}
+          >
             <input
               type="text"
               placeholder="검색어를 입력하세요"
